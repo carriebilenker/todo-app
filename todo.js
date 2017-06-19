d691f8bfec494cf29fae1128e28582d89bfbfe74
Carrie <carrie.bilenker@gmail.com>
2017-06-19 02:10:30 -0700

    styles forms, adds div for Tasks assigned, creates form handler on submit

---
+++ todo.js
@@ -0,0 +1,24 @@
+// function addTask(){
+// var taskList[0] = document.getElementByName("taskAssignment");
+// console.log(taskList[0].id);
+// }
+
+// trying to add new tasks in var taskList to the div taskBox
+window.onload = function(){
+  // var paras = document.querySelectorAll('p');
+  // for (var i=0; i<paras.length; i++){
+    // console.log(paras[i]);
+  var div = "taskBox".createUlNode("ul")
+
+  for(var i =0; i<neighborhood.houses.length; i++){
+      console.log(neighborhood.houses[i].roof)
+      // create li node
+      var li = document.createElement("li")
+      // add roof string to li
+      li.innerHTML = neighborhood.houses[i].ro
+
+var worker = {
+      "name":"Yakko",
+      "name":"Wakko",
+      "name":"Dot",
+    };
